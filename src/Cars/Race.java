35fe83abc24335b7f4d91c27b15c92061fb97f5d
Alex <7up-des@mail.ru>
2015-02-24 20:56:13 +0200

    tratata

--- src/Cars/Race.java
+++ src/Cars/Race.java
@@ -20,7 +20,7 @@ public class Race {
 
         for(int j=1;j<=laps;j++) {
             System.out.println("-----LAP"+j+"-----");
-			System.out.println("blablabla");
+			System.out.println("tratata");
             for (int i = 0; i < racer.length; i++) {
 
                 racer[i].drive();
